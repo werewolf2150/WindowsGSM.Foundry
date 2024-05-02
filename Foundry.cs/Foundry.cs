47689e0140cd5556175ab63c07c5de6565d39cf3
werewolf2150 <werewolf2150@hotmail.com>
2024-05-02 22:44:53 +0200

    Update Foundry.cs
    Exe and not bat

--- Foundry.cs/Foundry.cs
+++ Foundry.cs/Foundry.cs
@@ -35,7 +35,7 @@ namespace WindowsGSM.Plugins
 
 
         // - Game server Fixed variables
-        public override string StartPath => @"FoundryDedicatedServerLauncher.bat"; // Game server start path
+        public override string StartPath => @"FoundryDedicatedServer.exe"; // Game server start path
         public string FullName = "Foundry Dedicated Server"; // Game server FullName
         public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
         public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
