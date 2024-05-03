3af87ab403a13a8740a58b5416886343ce79333d
Alex <alex@abarthel.me>
2024-05-04 00:02:40 +0200

    use the actual data, not the defaults. use map parameter from Edit Config for MapSeed. Add randomized mapseed

--- Foundry.cs/Foundry.cs
+++ Foundry.cs/Foundry.cs
@@ -5,9 +5,7 @@ using WindowsGSM.Functions;
 using WindowsGSM.GameServer.Query;
 using WindowsGSM.GameServer.Engine;
 using System.IO;
-using System.Linq;
-using System.Net;
-
+using System.Text;
 
 namespace WindowsGSM.Plugins
 {
@@ -29,41 +27,45 @@ namespace WindowsGSM.Plugins
         public override string AppId => "2915550"; // Game server appId Steam
 
         // - Standard Constructor and properties
-        public Foundry(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
-        private readonly ServerConfig _serverData;
-        public string Error, Notice;
+        public Foundry(ServerConfig serverData) : base(serverData)
+        {
+            //create a random seed
+            serverData.ServerMap = new Random().Next().ToString();
+            base.serverData = serverData;
+        }
 
 
         // - Game server Fixed variables
         public override string StartPath => @".\FoundryDedicatedServer.exe"; // Game server start path
+        public string ConfigFile = "App.cfg";
         public string FullName = "Foundry Dedicated Server"; // Game server FullName
-        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+        public bool AllowsEmbedConsole = false;  // Only Embedd Console
         public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
 
         // TODO: Undisclosed method
         public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
 
         // - Game server default values
-		public string ServerName = "HappyPlace";
-		public string ServerWorldName = "MyFancyFactory";
+        public string ServerName = "HappyPlace";
         public string Port = "3724"; // Default port
-		public string ServerPassword = "only_friends"; //password to connect
-        public string Defaultmap = "MyFancyFactory"; // Default map name
-		public string DefaultPause = "true"; //  Will the server pause when nobody is connected.
-		public string DefaultAutoSaveInterval = "300"; // Sets the autosave frequency in seconds.
-		public string DefaultPublic = "true"; // Sets whether the server is listed on the Steam server browser.
-		public string DefaultMapSeed = "42938743982"; //  Sets the map seed used to generate the world.
-		public string DefaultFolder = "Server01"; // Sets the absolute folder where things like logs and save files will be stored. This is mostly used by server providers so that they can run multiple dedicated servers on a single machine.
+        public string QueryPort = "3724"; // Default port
+        public string Defaultmap = "1"; // Sets the map seed used to generate the world.
         public string Maxplayers = "32"; // Default maxplayers
-        
+
+        public string ServerPassword = "only_friends"; //password to connect
+        public string DefaultPause = "true"; //  Will the server pause when nobody is connected.
+        public string DefaultAutoSaveInterval = "300"; // Sets the autosave frequency in seconds.
+        public string DefaultPublic = "true"; // Sets whether the server is listed on the Steam server browser.
+        public string DefaultFolder = "Server01"; // Sets the absolute folder where things like logs and save files will be stored. This is mostly used by server providers so that they can run multiple dedicated servers on a single machine.
         // TODO: Unsupported option
 
         // TODO: May not support
         public string Additional = ""; // Additional server start parameter
 
 
+
         // - Create a default cfg for the game server after installation
-		public async void CreateAppCFG()
+        public async void CreateAppCFG()
 		{
 			string fileName = "app.cfg";
 			// Chemin complet du fichier
@@ -78,7 +80,7 @@ namespace WindowsGSM.Plugins
 			writer.WriteLine($"//Available Options:");
 			writer.WriteLine($"//server_world_name");
 			writer.WriteLine($"//Sets the server world name. This is the folder where the save files will be stored.");
-			writer.WriteLine($"server_world_name={ServerWorldName}");
+			writer.WriteLine($"server_world_name={serverData.ServerName}");
 			writer.WriteLine($"//server_password");
 			writer.WriteLine($"//Sets the server password.");
 			writer.WriteLine($"server_password={ServerPassword}");
@@ -93,19 +95,19 @@ namespace WindowsGSM.Plugins
 			writer.WriteLine($"server_is_public={DefaultPublic}");
 			writer.WriteLine($"//server_port");
 			writer.WriteLine($"//Sets the network port used by the game. Default is 3724.");
-			writer.WriteLine($"server_port={Port}");
+			writer.WriteLine($"server_port={serverData.ServerPort}");
 			writer.WriteLine($"//map_seed");
 			writer.WriteLine($"//Sets the map seed used to generate the world.");
-			writer.WriteLine($"map_seed={DefaultMapSeed}");
+			writer.WriteLine($"map_seed={serverData.ServerMap}");
 			writer.WriteLine($"//server_persistent_data_override_folder");
 			writer.WriteLine($"//Sets the absolute folder where things like logs and save files will be stored. This is mostly used by server providers so that they can run multiple dedicated servers on a single machine.");
 			writer.WriteLine($"server_persistent_data_override_folder={DefaultFolder}");
 			writer.WriteLine($"//server_name");
 			writer.WriteLine($"//This is the name of the server listed in the Steam server browser.");
-			writer.WriteLine($"server_name={ServerName}");
+			writer.WriteLine($"server_name={serverData.ServerName}");
 			writer.WriteLine($"//server_max_players");
 			writer.WriteLine($"//This sets the max amount of players on a server.");
-			writer.WriteLine($"server_max_players={Maxplayers}");
+			writer.WriteLine($"server_max_players={serverData.ServerMaxPlayer}");
 			}	
 
 			Console.WriteLine("Fichier app.cfg créé avec succès !");
@@ -115,21 +117,74 @@ namespace WindowsGSM.Plugins
 				Console.WriteLine($"Une erreur s'est produite : {ex.Message}");
 			}
 		}
+        public void UpdateCFG()
+        {
+            // Chemin complet du fichier
+            string filePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, ConfigFile);
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    if (line.Contains("server_world_name"))
+                    {
+                        sb.Append($"server_world_name={serverData.ServerName}");
+                        continue;
+                    }
+                    if (line.Contains("server_port"))
+                    {
+                        sb.Append($"server_port={serverData.ServerPort}");
+                        continue;
+                    }
+                    if (line.Contains("server_name"))
+                    {
+                        sb.Append($"server_name={serverData.ServerName}");
+                        continue;
+                    }
+                    if (line.Contains("map_seed"))
+                    {
+                        sb.Append($"map_seed={serverData.ServerMap}");
+                        continue;
+                    }
+                    if (line.Contains("server_max_players"))
+                    {
+                        sb.Append($"server_max_players={serverData.ServerMaxPlayer}");
+                        continue;
+                    }
+
+                    sb.Append(line);
+
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+
+                File.WriteAllText(filePath, sb.ToString());
+
+                var serverConsole = new ServerConsole(serverData.ServerID);
+                serverConsole.Add($"ConfigFile {ConfigFile} successfully updated!");
+            }
+            catch (Exception ex)
+            {
+                Error = $"Error Occured : {ex.Message}";
+            }
+        }
 
 
         // - Start server function, return its Process to WindowsGSM
         public async Task<Process> Start()
         {
-            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, StartPath);
             if (!File.Exists(shipExePath))
             {
                 Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
                 return null;
             }
-
+            UpdateCFG();
             // Prepare start parameter
             string param = "FactoryGame -log -unattended";
-            param += $" {_serverData.ServerParam}";
+            param += $" {serverData.ServerParam}";
             //param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -Port={_serverData.ServerPort}"; 
             //param += string.IsNullOrWhiteSpace(_serverData.ServerQueryPort) ? string.Empty : $" -ServerQueryPort={_serverData.ServerQueryPort}";
             //param += string.IsNullOrWhiteSpace(_serverData.ServerIP) ? string.Empty : $" -Multihome={_serverData.ServerIP}";
@@ -139,7 +194,7 @@ namespace WindowsGSM.Plugins
             {
                 StartInfo =
                 {
-                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    WorkingDirectory = ServerPath.GetServersServerFiles(serverData.ServerID),
                     FileName = shipExePath,
                     Arguments = param,
                     WindowStyle = ProcessWindowStyle.Normal,
@@ -155,7 +210,7 @@ namespace WindowsGSM.Plugins
                 p.StartInfo.RedirectStandardInput = true;
                 p.StartInfo.RedirectStandardOutput = true;
                 p.StartInfo.RedirectStandardError = true;
-                var serverConsole = new ServerConsole(_serverData.ServerID);
+                var serverConsole = new ServerConsole(serverData.ServerID);
                 p.OutputDataReceived += serverConsole.AddOutput;
                 p.ErrorDataReceived += serverConsole.AddOutput;
             }
