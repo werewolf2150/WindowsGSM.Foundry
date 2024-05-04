700c60b9f144f89415c076254b045368551a5cf2
werewolf2150 <werewolf2150@hotmail.com>
2024-05-04 03:18:50 +0200

    Update Foundry.cs
    Update for correction to launch server after install
    Update CFG desabled because crash detection plugin in Wgsm

--- Foundry.cs/Foundry.cs
+++ Foundry.cs/Foundry.cs
@@ -5,7 +5,9 @@ using WindowsGSM.Functions;
 using WindowsGSM.GameServer.Query;
 using WindowsGSM.GameServer.Engine;
 using System.IO;
-using System.Text;
+using System.Linq;
+using System.Net;
+
 
 namespace WindowsGSM.Plugins
 {
@@ -27,29 +29,33 @@ namespace WindowsGSM.Plugins
         public override string AppId => "2915550"; // Game server appId Steam
 
         // - Standard Constructor and properties
-        public Foundry(ServerConfig _serverData) : base(_serverData) => serverData = _serverData;
+		public Foundry(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        //public string Error, Notice;
 
+        // - Game server default values
+        public string Port = "3724"; // Default port
+        public string QueryPort = "3725"; // Default query port. This is the port specified in the Server Manager in the client UI to establish a server connection.
+        public string BeaconPort = "3726"; // Default beacon port. This port currently cannot be set freely.
+		//
+		public string ServerName = "HappyPlace";
+		public string ServerWorldName = "MyFancyFactory";
+		public string ServerPassword = "only_friends"; //password to connect
+        public string Defaultmap = new Random().Next().ToString(); // Sets the map seed used to generate the world.
+		public string DefaultPause = "true"; //  Will the server pause when nobody is connected.
+		public string DefaultAutoSaveInterval = "300"; // Sets the autosave frequency in seconds.
+		public string DefaultPublic = "true"; // Sets whether the server is listed on the Steam server browser.
+        public string Maxplayers = "32"; // Default maxplayers
+		
         // - Game server Fixed variables
         public override string StartPath => @".\FoundryDedicatedServer.exe"; // Game server start path
-        public string ConfigFile = "App.cfg";
         public string FullName = "Foundry Dedicated Server"; // Game server FullName
-        public bool AllowsEmbedConsole = false;  // Only Embedd Console
+		public string ConfigFile = "app.cfg";
+        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
         public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
 
         // TODO: Undisclosed method
         public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
-
-        // - Game server default values
-        public string ServerName = "HappyPlace";
-        public string Port = "3724"; // Default port
-        public string QueryPort = "3725"; // Default port
-        public string Defaultmap = new Random().Next().ToString(); // Sets the map seed used to generate the world.
-        public string Maxplayers = "32"; // Default maxplayers
-
-        public string ServerPassword = "only_friends"; //password to connect
-        public string DefaultPause = "true"; //  Will the server pause when nobody is connected.
-        public string DefaultAutoSaveInterval = "300"; // Sets the autosave frequency in seconds.
-        public string DefaultPublic = "true"; // Sets whether the server is listed on the Steam server browser.
 
         // TODO: Unsupported option
 
@@ -58,17 +64,18 @@ namespace WindowsGSM.Plugins
 
 
         // - Create a default cfg for the game server after installation
-        public async void CreateServerCFG()
-        {
-            // Chemin complet du fichier
-            string filePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, ConfigFile);
-
-            try
-            {
-                // Créer le fichier
-                using (StreamWriter writer = new StreamWriter(filePath))
-                {
-                    // Écrire les informations dans le fichier
+		public async void CreateServerCFG()
+		{
+			// Chemin complet du fichier
+			//string filePath = Path.Combine(Environment.CurrentDirectory, ConfigFile);
+			string filePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, ConfigFile);
+			
+			try
+			{
+				// Créer le fichier
+				using (StreamWriter writer = new StreamWriter(filePath))
+				{
+					// Écrire les informations dans le fichier
                     writer.WriteLine($"//Available Options:");
                     writer.WriteLine($"//server_world_name");
                     writer.WriteLine($"//Sets the server world name. This is the folder where the save files will be stored.");
@@ -100,18 +107,18 @@ namespace WindowsGSM.Plugins
                     writer.WriteLine($"//server_max_players");
                     writer.WriteLine($"//This sets the max amount of players on a server.");
                     writer.WriteLine($"server_max_players={serverData.ServerMaxPlayer}");
-                    writer.Close();
-                }
+					writer.Close();
+				}	
                 Directory.CreateDirectory(Functions.ServerPath.GetServersServerFiles(serverData.ServerID, "Mods"));
-                Notice = $"ConfigFile {ConfigFile} successfully created!";
-            }
-            catch (Exception ex)
-            {
-                Error = $"Error Occured : {ex.Message}";
-            }
-        }
-
-        public void UpdateCFG()
+				Console.WriteLine("Fichier app.cfg créé avec succès !");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Une erreur s'est produite : {ex.Message}");
+			}
+		}
+		/*
+		public void UpdateCFG()
         {
             // Chemin complet du fichier
             string filePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, ConfigFile);
@@ -168,22 +175,21 @@ namespace WindowsGSM.Plugins
             {
                 Error = $"Error Occured : {ex.Message}";
             }
-        }
-
+        }*/
 
         // - Start server function, return its Process to WindowsGSM
         public async Task<Process> Start()
         {
-            string shipExePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, StartPath);
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
             if (!File.Exists(shipExePath))
             {
                 Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
                 return null;
             }
-            UpdateCFG();
+
             // Prepare start parameter
             string param = "FactoryGame -log -unattended";
-            param += $" {serverData.ServerParam}";
+            param += $" {_serverData.ServerParam}";
             //param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -Port={_serverData.ServerPort}"; 
             //param += string.IsNullOrWhiteSpace(_serverData.ServerQueryPort) ? string.Empty : $" -ServerQueryPort={_serverData.ServerQueryPort}";
             //param += string.IsNullOrWhiteSpace(_serverData.ServerIP) ? string.Empty : $" -Multihome={_serverData.ServerIP}";
@@ -193,30 +199,36 @@ namespace WindowsGSM.Plugins
             {
                 StartInfo =
                 {
-                    WorkingDirectory = ServerPath.GetServersServerFiles(serverData.ServerID),
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
                     FileName = shipExePath,
                     Arguments = param,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    RedirectStandardError = true,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
+                    WindowStyle = ProcessWindowStyle.Normal,
+                    CreateNoWindow = false,
+                    UseShellExecute = false
                 },
                 EnableRaisingEvents = true
             };
 
-            var serverConsole = new ServerConsole(serverData.ServerID);
-            p.OutputDataReceived += serverConsole.AddOutput;
-            p.ErrorDataReceived += serverConsole.AddOutput;
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
 
             // Start Process
             try
             {
                 p.Start();
-                p.BeginOutputReadLine();
-                p.BeginErrorReadLine();
-
+                if (AllowsEmbedConsole)
+                {
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                }
                 return p;
             }
             catch (Exception e)
@@ -227,20 +239,18 @@ namespace WindowsGSM.Plugins
         }
 
 
-        // - Stop server function
+// - Stop server function
         public async Task Stop(Process p)
         {
             await Task.Run(() =>
             {
                 Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
                 Functions.ServerConsole.SendWaitToMainWindow("^c");
-                p.WaitForExit(7000);
-                p.Kill();
+                p.WaitForExit(20000);
             });
         }
 
-
-        // fixes WinGSM bug, https://github.com/WindowsGSM/WindowsGSM/issues/57#issuecomment-983924499
+// fixes WinGSM bug, https://github.com/WindowsGSM/WindowsGSM/issues/57#issuecomment-983924499
         public async Task<Process> Update(bool validate = false, string custom = null)
         {
             var (p, error) = await Installer.SteamCMD.UpdateEx(serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
